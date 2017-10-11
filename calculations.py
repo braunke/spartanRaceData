60fbf1ccbb48057e956ea620d6d309ea42e27609
htks <kbsd@hotmail.com>
2017-10-11 16:53:03 -0500

    more calculations

--- calculations.py
+++ calculations.py
@@ -35,7 +35,7 @@ def getAInfo(race):
     for row in cursor:
         location = (row[0])
     return final, location
-#getAInfo()
+
 #collects all the data and puts it in a list
 def getItAll():
     conn = sqlite3.connect('sp.db')
@@ -50,3 +50,75 @@ def getItAll():
         races.append(race)
     return races
 getItAll()
+#calculates the average race times for males and females
+def getAverageGender(race, gender):
+    conn = sqlite3.connect('sp.db')
+    cur = conn.cursor()
+    race = race
+    gender = gender
+    cur.execute('''SELECT TIME  from
+                                    RESULTS WHERE RACEID=? and GENDER=?''', (race,gender,))
+    results = cur.fetchall()
+    total = []
+    for rows in results:
+        result = (rows[0])
+        split = result.split(':')
+        # handles if a time does not have hours
+        if len(split) < 3:
+            split.insert(0, '0')
+            s = ":"
+            result = s.join(split)
+        total.append(result)
+    # used this for help with working with times
+    # https://stackoverflow.com/questions/12033905/using-python-to-create-an-average-out-of-a-list-of-times
+    times = total
+    average = (str(timedelta(seconds=sum(
+        map(lambda f: int(f[0]) * 3600 + int(f[1]) * 60 + int(f[2]), map(lambda f: f.split(':'), times))) / len(
+        times))))
+    # rounds to whole seconds
+    averageS = average[:average.rfind(".")]
+    ftr = [3600, 60, 1]
+    times = sum([a * b for a, b in zip(ftr, map(int, averageS.split(':')))])
+    final = times / 3600
+    conn = sqlite3.connect('sp.db')
+    cursor = conn.execute('''SELECT LOCATION from
+                                        RACES WHERE RACEID=?''', (race,))
+    location = ''
+    for row in cursor:
+        location = (row[0])
+    return final, location
+def getAllMale():
+    conn = sqlite3.connect('sp.db')
+    cur = conn.cursor()
+    cur.execute('''SELECT RACEID  from
+                                  RACES ''', )
+    gender = "M"
+    raceids = cur.fetchall()
+    maleRaces = []
+    for raceid in raceids:
+        race = getAverageGender(raceid[0], gender,)
+        maleRaces.append(race)
+    return maleRaces
+def getAllFemale():
+    conn = sqlite3.connect('sp.db')
+    cur = conn.cursor()
+    cur.execute('''SELECT RACEID  from
+                                  RACES ''', )
+    raceids = cur.fetchall()
+    gender = "F"
+    maleRaces = []
+    for raceid in raceids:
+        race = getAverageGender(raceid[0], gender)
+        maleRaces.append(race)
+    return maleRaces
+def getRaceList():
+    conn = sqlite3.connect('sp.db')
+    cursor = conn.execute('''SELECT LOCATION from
+                                      RACES WHERE EXISTS ''',)
+    location = ''
+    for row in cursor:
+        location = (row[0])
+    print(location)
+    return  location
+getRaceList()
+
--- graph.py
+++ graph.py
@@ -8,3 +8,15 @@ def averageTimesGraph():
         line_chart.add(race[1], race[0])
     line_chart.render_to_file('averageBar.svg')
 averageTimesGraph()
+def averageMvsF():
+    maleRace = calculations.getAllMale()
+    femaleRace = calculations.getAllFemale()
+    line_chart = pygal.Bar()
+    line_chart.title = 'Average TImes Males vs Female'
+    line_chart.x_labels = map(str, range(2002, 2013))
+    for mRace in maleRace:
+        line_chart.add(mRace[1], mRace[0])
+    for fRace in femaleRace:
+        line_chart.add(fRace[1], fRace[0])
+    line_chart.render()
+averageMvsF()
