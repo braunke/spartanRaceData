0c5dfff82b2f5869cbc7cf4e4141030cd15e9163
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-10-16 10:12:39 -0500

    fine tuned graphs

--- graph.py
+++ graph.py
@@ -2,40 +2,37 @@ import pygal
 import calculationsForAverages
 import calculationsForScatter
 import calculationsForAgeGroups
-import topTimes
+import calculationsTopTimes
 #used this site for help with pygal
 #http://www.pygal.org/en/stable/documentation/types/xy.html#scatter-plot
+#graph of average race finish times
 def averageTimesGraph():
     races = calculationsForAverages.getItAll()
-    line_chart = pygal.HorizontalBar()
-    line_chart.title = 'Average race finish times'
+    line_chart = pygal.HorizontalBar(title='Average race finish times', x_title='Hours')
     for race in races:
         line_chart.add(race[1], race[0])
-    line_chart.render_to_file('averageBar.svg')
-averageTimesGraph()
+    line_chart.render_to_file('averageBar.html')
+#averageTimesGraph()
 def averageMvsF():
     maleRace = calculationsForAverages.getAllMale()
     femaleRace = calculationsForAverages.getAllFemale()
     locations = calculationsForAverages.getRaceList()
-    line_chart = pygal.Bar()
-    line_chart.title = 'Average Times Males vs Female'
+    line_chart = pygal.Bar(title='Average Times Males vs Female', x_title='Races', y_title='Hours', x_label_rotation=20)
     line_chart.x_labels = locations
-
     line_chart.add('Males', maleRace)
     line_chart.add('Females', femaleRace)
     line_chart.render_to_file('averageMFBar.svg')
-averageMvsF()
+#averageMvsF()
 def elevationScatter():
-    xy_chart = pygal.XY(stroke=False)
+    xy_chart = pygal.XY(title='Affects of Elevation', x_title='Hours', y_title='Elevation')
     locations = calculationsForAverages.getRaceList()
-    xy_chart.title = 'Affects of Elevation'
     for i in range(0, 10):
         xy_chart.add(locations[i], calculationsForScatter.getItAllAlt(i))
     xy_chart.render_to_file('elevationScatter.svg')
 #elevationScatter()
 def ageGroupPie():
     pie_chart = pygal.Pie()
-    pie_chart.title = 'Age group categories (in %)'
+    pie_chart.title = 'Age group categories'
     pie_chart.add('Under 20', calculationsForAgeGroups.getAllAgeGroup(0,19))
     pie_chart.add('20-29', calculationsForAgeGroups.getAllAgeGroup(20,29))
     pie_chart.add('30-39', calculationsForAgeGroups.getAllAgeGroup(30,39))
@@ -44,27 +41,24 @@ def ageGroupPie():
     pie_chart.add('60-69', calculationsForAgeGroups.getAllAgeGroup(60, 69))
     pie_chart.add('70-79', calculationsForAgeGroups.getAllAgeGroup(70, 79))
     pie_chart.render_to_file('agePie.svg')
-ageGroupPie()
+#ageGroupPie()
 def humidityScatter():
-    xy_chart = pygal.XY(stroke=False)
+    xy_chart = pygal.XY( title='Affects of Humidity', x_title='Hours', y_title='Humidity')
     locations = calculationsForAverages.getRaceList()
-    xy_chart.title = 'Affects of Humidity'
     for i in range(0, 10):
         xy_chart.add(locations[i], calculationsForScatter.getItAllHumidity(i))
     xy_chart.render_to_file('humidityScatter.svg')
 #humidityScatter()
 def tempScatter():
-    xy_chart = pygal.XY(stroke=False)
+    xy_chart = pygal.XY(title='Affects of Temperature', x_title='Hours', y_title='Temperature')
     locations = calculationsForAverages.getRaceList()
-    xy_chart.title = 'Affects of Temperature'
     for i in range(0, 10):
         xy_chart.add(locations[i], calculationsForScatter.getItAllTemp(i))
     xy_chart.render_to_file('tempScatter.svg')
-tempScatter()
+#tempScatter()
 def topTimesBar():
-    races = topTimes.getItAll()
-    line_chart = pygal.HorizontalBar()
-    line_chart.title = 'Average race finish times'
+    races = calculationsTopTimes.getItAll()
+    line_chart = pygal.HorizontalBar(title='Top 20 Average Race Finish Times', x_title='Hours')
     for race in races:
         line_chart.add(race[1], race[0])
     line_chart.render_to_file('topTimesBar.svg')
