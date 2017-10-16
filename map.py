b59fc8391f3f921dadfa2b2bae9a4ff7fded7cf9
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-10-15 19:09:52 -0500

    start on maps

--- map.py
+++ map.py
@@ -1,4 +1,39 @@
 import folium
+m = folium.Map(
+    location=[33.6681, 117.3273],
+    zoom_start=12,
+    tiles='Stamen Terrain'
+)
+
+folium.Marker(
+    location=[33.6681, 117.3273],
+    popup='California',
+    icon=folium.Icon(icon='cloud')
+).add_to(m)
+
+folium.Marker(
+    location=[33.5802, 11.6462],
+    popup='Arizona',
+    icon=folium.Icon(color='green')
+).add_to(m)
+
+folium.Marker(
+    location=[30.0974, 96.0783],
+    popup='Texas',
+    icon=folium.Icon(color='red', icon='info-sign')
+).add_to(m)
+folium.Marker(
+    location=[33.67129, -83.94149],
+    popup='Georgia',
+    icon=folium.Icon(color='red', icon='info-sign')
+).add_to(m)
+folium.Marker(
+    location=[47.8289, 122.0149],
+    popup='Washington',
+    icon=folium.Icon(color='red', icon='info-sign')
+).add_to(m)
 
 
-m = folium.Map(location=[45.5236, -122.6750])
+
+
+m.save('index.html')
